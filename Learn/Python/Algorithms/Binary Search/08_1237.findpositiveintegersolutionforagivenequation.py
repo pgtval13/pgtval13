0cfbbbd38b24c9200931580471516186920c6dc6
pgtval13 <93081150+pgtval13@users.noreply.github.com>
2021-11-09 15:29:52 +0800

    1237. Find Positive Integer Solution for a Given Equation

---
+++ Learn/Python/Algorithms/Binary Search/08_1237.findpositiveintegersolutionforagivenequation.py
@@ -0,0 +1,17 @@
+""" 
+Problem 1237. Find Positive Integer Solution for a Given Equation
+
+Input:
+1. function f(x,y)
+2. z
+
+Output:
+1. all positive integer pairs x and y where f(x,y) == z
+
+Constraints:
+1. 1 <= function_id <= 9
+2. 1 <= z <= 100
+3. It is guaranteed that the solutions of f(x, y) == z will be in the range 1 <= x, y <= 1000.
+4. It is also guaranteed that f(x, y) will fit in 32 bit signed integer if 1 <= x, y <= 1000.
+
+"""
